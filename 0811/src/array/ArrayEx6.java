f63e5c10c29ff915b2499e4b43f8dcd5bf75c6be
siu2388 <111097199+siu2388@users.noreply.github.com>
2023-08-11 17:52:12 +0900

    배열

--- 0811/src/array/ArrayEx6.java
+++ 0811/src/array/ArrayEx6.java
@@ -8,14 +8,14 @@ public class ArrayEx6 {
 		int[] coinUnit = { 500, 100, 50, 10 };
 		int money = 2680;
 		System.out.println("money=" + money);
-
-		for (int i = 0; i < coinUnit.length; i++) {
-
-			money = (money / coinUnit[i]) - coinUnit[i];
-			if (money < coinUnit[i])
-
-				System.out.println(coinUnit[i] + "원 : " + (money / coinUnit[i]));
-			break;
+//
+//		for (int i = 0; i < coinUnit.length; i++) {
+//
+//			money = (money / coinUnit[i]) - coinUnit[i];
+//			if (money < coinUnit[i])
+//
+//				System.out.println(coinUnit[i] + "원 : " + (money / coinUnit[i]));
+//			break;
 		}
 	}
 
