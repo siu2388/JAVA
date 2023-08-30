2d0fb0200c547e62056507dfdbd4956d6b8adf5e
siu2388 <swear428@naver.com>
2023-08-30 23:18:09 +0900

    0830과제

--- 0830/src/ex11/Ex11_2.java
+++ 0830/src/ex11/Ex11_2.java
@@ -1,9 +1,40 @@
 package ex11;
 
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.HashSet;
+import java.util.TreeSet;
+
 public class Ex11_2 {
 
 	public static void main(String[] args) {
+		ArrayList list = new ArrayList();
+		list.add(3);
+		list.add(6);
+		list.add(2);
+		list.add(2);
+		list.add(2);
+		list.add(7);
 
+		HashSet set = new HashSet(list); // [2,3,6,7] &중복허용X,sorting
+		TreeSet tset = new TreeSet(list);
+		Stack stack = new Stack();
+		stack.addAll(tset);
+
+		while (!stack.empty()) {
+			System.out.println(stack.pop()); // stack에 저장된 값을 하나씩 꺼낸다.
+		}
+		
 	}
 
 }
+
+/**
+ * HashSet 
+ * 중복허용X, 저장순서 유지 안함
+ * 
+ * TreeSet
+ * 정렬해서 저장(default 오름차순 : 2,3,6,7)
+ * 
+ * Stack -> FILO
+ */
