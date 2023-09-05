861bf86cc1a88144da772baf689daed4e357c1bc
siu2388 <111097199+siu2388@users.noreply.github.com>
2023-09-06 08:57:20 +0900

    0901

--- 0901/src/ObjectStreamTest.java
+++ 0901/src/ObjectStreamTest.java
@@ -1,7 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class ObjectStreamTest {
+	static void write(List<Person> pers) {
+
+	}
 
 	public static void main(String[] args) {
+
+		List<Person> people = new ArrayList<>();
+		people.add(new Person("kim", 23, 44, false));
+		people.add(new Person("lee", 23, 44, false));
+		people.add(new Person("park", 23, 44, false));
+		people.add(new Person("go", 23, 44, false));
+		write(people);
 
 	}
 
