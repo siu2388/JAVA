5256e3b9b1ea935c322751f69d79905b5bb7c96a
siu2388 <111097199+siu2388@users.noreply.github.com>
2023-09-07 17:17:26 +0900

    0907

--- JavaSelf/src/chapter03/Ex3_2.java
+++ JavaSelf/src/chapter03/Ex3_2.java
@@ -3,7 +3,28 @@ package chapter03;
 public class Ex3_2 {
 
 	public static void main(String[] args) {
+		int i = 5, j = 0;
 
+		j = i++; // j에 5가 대입이 되고, i가 6이 됨
+
+		System.out.println("j=" + j);
+		System.out.println("i=" + i);
+
+		i = 5;
+		j = 0;
+
+		j = ++i; // i가 6으로 증가한 후에 j에 대입이 됨
+
+		System.out.println("j=" + j);
+		System.out.println("i=" + i);
+
+		i = 5;
+		j = 5;
+
+		System.out.println(i++); // 5
+		System.out.println(++j); // 6
+		System.out.println("j=" + j);
+		System.out.println("i=" + i);
 	}
 
 }
